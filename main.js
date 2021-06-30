4db0a1558af1e4869cbccd449060630567d73c6e
Kamil Dudek <kamil.dudek.1337@gmail.com>
2021-06-30 12:02:31 +0200

    Minor improvements and background bugfix

--- main.js
+++ main.js
@@ -5,11 +5,6 @@ var objects = [];
 
 var divs = document.getElementsByClassName("dev");
 
-/*
-for (i = 0; i < divs.length; ++i) {
-    divs[i].addEventListener("click", myFunc);
-}
-*/
 document.addEventListener("click", printMousePos);
 
 $.repeat().add('connection').each($).connections('update').wait(0);
